347e894bccbc5b11d3de31cad0c9eac155154d6f
dixeris <hs031545@gmail.com>
2026-04-05 20:28:48 +0900

    add L2 mac resolution logic

--- vnic.c
+++ vnic.c
@@ -9,9 +9,9 @@
 #include <linux/ip.h>
 #include <linux/u64_stats_sync.h>
 #include <linux/net_tstamp.h>
+#include <linux/inet.h>
 #include <net/rtnetlink.h>
 #include <net/icmp.h>
-
 #define DRV_NAME	"vnic"
 
 
@@ -21,26 +21,110 @@ struct vnic_private {
 	struct net_device *fwd_device;
 };
 	
-static __always_inline void swap_src_dst_mac(void *data)
+static __always_inline void swap_src_dst_mac(void *data, void *dst_ha)
 {
 	unsigned short *p = data;
-	unsigned short dst[3];
+	unsigned short *dst = dst_ha;
 
-	dst[0] = p[0];
-	dst[1] = p[1];
-	dst[2] = p[2];
-	p[0] = p[3];
-	p[1] = p[4];
-	p[2] = p[5];
-	p[3] = dst[0];
-	p[4] = dst[1];
-	p[5] = dst[2];
+	p[3] = p[0];
+	p[4] = p[1];
+	p[5] = p[2];
+
+	p[0] = dst[0];
+	p[1] = dst[1];
+	p[2] = dst[2];
 }
 	
 static void vnic_get_stats64(struct net_device *dev, 
 				struct rtnl_link_stats64 *stats)
 {
 	dev_get_tstats64(dev, stats);
+}
+
+/* * TODO: Implement packet forwarding logic
+ * 1. flowi4 init -> 2. routing (ip_route_output_key) 
+ * 3. neigh lookup (ip_neigh_for_gw) -> 4. MAC extraction (neigh->ha) 
+ * 5. L2 encapsulation (eth_header) -> 6. Transmission (dev_queue_xmit)
+*/
+static int get_nxthop_ha(struct sk_buff *skb, void *dst_ha, 
+		struct iphdr *iph, struct net_device *fwd_device)
+{
+	int ret = -1;
+	struct rtable *rt;
+	struct net *net = dev_net(fwd_device);
+	struct neighbour *neigh; 
+	struct flowi4 fl4;
+
+	//flowi4 init 
+	memset(&fl4, 0, sizeof(fl4));
+
+	fl4.daddr = iph->daddr;
+	fl4.saddr = iph->saddr;         
+	fl4.flowi4_oif = fwd_device->ifindex; 
+	fl4.flowi4_mark = skb->mark;
+	fl4.flowi4_uid = sock_net_uid(net, NULL);
+	fl4.flowi4_dscp = ip4h_dscp(ip_hdr(skb));
+	fl4.flowi4_proto = IPPROTO_ICMP;
+
+	//get rtable for next hop 
+	rt = ip_route_output_key(net, &fl4);
+
+	if (IS_ERR(rt))
+		return ret;
+
+	//ARP(neighbour) lookup 
+
+	neigh = ip_neigh_for_gw(rt, NULL, NULL);
+	if (!IS_ERR(neigh)) {
+		memcpy(dst_ha, neigh->ha, ETH_ALEN);
+	        neigh_release(neigh);
+		ret = 0;
+	}
+
+	ip_rt_put(rt);
+	return ret; 
+}
+
+static int handle_ping_reply(struct sk_buff *skb, struct net_device *dev,
+					struct net_device *fwd_device) 
+{
+	int ret;
+	struct ethhdr *eth = eth_hdr(skb);
+	struct icmphdr *icmph = icmp_hdr(skb);
+	struct iphdr *iph = ip_hdr(skb);
+	unsigned char dst_ha[ETH_ALEN];
+
+
+	//change to ECHOREPLY
+	icmph->type	=	ICMP_ECHOREPLY;
+	
+	//ip address swap
+	swap(iph->saddr,iph->daddr);
+	
+	//MAC address setting 
+	if(!get_nxthop_ha(skb, dst_ha, iph, fwd_device)) {
+		swap_src_dst_mac(eth, dst_ha);
+	}
+
+
+	//checksum recalculatation	
+	
+	//ip checksum 
+	iph->check = 0;
+	ip_send_check(iph);
+
+	//icmp(transport layer) checksum 
+	icmph->checksum = 0;
+	//icmphdr checksum size is 16bit. we have to fold 32-bit checksum 
+	//to 16bit 
+	icmph->checksum = csum_fold(skb_checksum(skb, ip_hdrlen(skb), 
+				skb->len - ip_hdrlen(skb), 0));
+	
+	//send 
+	skb->dev	=	fwd_device;
+
+	ret = dev_queue_xmit(skb);
+	return ret;
 }
 
 static netdev_tx_t vnic_xmit(struct sk_buff *skb, struct net_device *dev)
@@ -60,6 +144,7 @@ static netdev_tx_t vnic_xmit(struct sk_buff *skb, struct net_device *dev)
 	//is this packet headed to myself or outside?
 	if(iph->protocol == IPPROTO_ICMP && iph->daddr == in_aton("10.0.0.1")) 
 	{
+		pr_info("10.0.0.1 icmp\n");
 		ret = handle_ping_reply(skb,dev,priv->fwd_device);
 		if(likely(ret == NET_XMIT_SUCCESS || ret == NET_XMIT_CN)) {
 			dev_sw_netstats_tx_add(dev, 1, len);
@@ -86,41 +171,6 @@ drop:
 	dev_kfree_skb(skb);
 	printk(KERN_INFO "network loop occur for vnic! Drop the packet\n");
 	return NETDEV_TX_OK;
-}
-
-static int handle_ping_reply(struct skb_buff *skb, struct net_device *dev,
-					struct net_device *fwd_device) 
-{
-	int ret;
-	struct ethhdr *eth = eth_hdr(skb);
-	struct icmphdr *icmph = icmp_hdr(skb);
-
-	//change to ECHOREPLY
-	icmph->type	=	ICMP_ECHOREPLY;
-	
-	//ip address swap
-	swap(iph->saddr,iph->daddr);
-	
-	//MAC address swap 
-	swap_src_dst_mac(eth);
-
-	//checksum recalculatation	
-	
-	//ip checksum 
-	ip_send_check(iph);
-
-	//icmp(transport layer) checksum 
-	icmph->checksum = 0;
-	//icmphdr checksum size is 16bit. we have to fold 32-bit checksum 
-	//to 16bit 
-	icmph->checksum = csum_fold(skb_checksum(skb, ip_hdrlen(skb), 
-				skb->len - ip_hdrlen(skb), 0));
-	
-	//send 
-	skb->dev	=	fwd_device;
-
-	ret = dev_queue_xmit(skb);
-	return ret;
 }
 
 
@@ -206,3 +256,5 @@ MODULE_LICENSE("GPL");
 module_init(vnic_init_module);
 module_exit(vnic_cleanup);
 
+//TODO  add delink logic(dev_put)
+//thinking about skb_cloned() 
